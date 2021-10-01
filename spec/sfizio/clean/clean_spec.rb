773f7c509a33a88b630c4582f7bed7015aad5324
Sebastian Shanus <sebastian.shanus@onepeloton.com>
2021-10-01 13:12:58 -0400

    Enable passing a nil Brewfile on clean

--- spec/sfizio/clean/clean_spec.rb
+++ spec/sfizio/clean/clean_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Sfizio::Clean do
-  context 'when clean is invoked' do
+  context 'when clean is invoked with a Brewfile' do
     let(:brewfile) { Sfizio::Brewfile.new([Sfizio::Formula.new('cloc', '2.9', nil)]) }
     let(:cleaner) { described_class.new(Sfizio::TAP_PATH, brewfile) }
     it 'uninstalls all Brewfile formula' do
@@ -13,6 +13,16 @@ describe Sfizio::Clean do
     it 'untaps the local tap' do
       expect(Sfizio::Brew::Tap).to receive(:untap).with(Sfizio::TAP_PATH, force: true).and_return(nil)
       allow(Sfizio::Brew::Uninstall).to receive(:formula).and_return(nil)
+      cleaner.clean!
+    end
+  end
+
+  context 'when clean is invoked without a Brewfile' do
+    let(:cleaner) { described_class.new(Sfizio::TAP_PATH, nil) }
+
+    it 'accepts a nil Brewfile' do
+      expect(Sfizio::Brew::Tap).to receive(:untap).with(Sfizio::TAP_PATH, force: true).and_return(nil)
+      expect(Sfizio::Brew::Uninstall).not_to receive(:formula)
       cleaner.clean!
     end
   end
